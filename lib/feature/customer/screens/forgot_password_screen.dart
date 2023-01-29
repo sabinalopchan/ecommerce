210ea062592bf0852eef96cbad1607ee8a257aca
unknown <sabinalopchan321@gmail.com>
2023-01-29 12:05:49 +0545

    reset password

--- lib/feature/customer/screens/forgot_password_screen.dart
+++ lib/feature/customer/screens/forgot_password_screen.dart
@@ -1,6 +1,12 @@
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:ecommerce/viewmodels/auth_viewmodel.dart';
+import 'package:ecommerce/viewmodels/global_ui_viewmodel.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import '../widgets/forgot_password_widget.dart';
+import '../../../common/app/theme.dart';
+import '../../../common/button/custom_button.dart';
+import '../../../common/icon/custom_icon.dart';
+import '../../../common/textfield/custom_textfield.dart';
 
 class ForgotPasswordSreen extends StatefulWidget {
   const ForgotPasswordSreen({super.key});
@@ -10,8 +16,93 @@ class ForgotPasswordSreen extends StatefulWidget {
 }
 
 class _ForgotPasswordSreenState extends State<ForgotPasswordSreen> {
+  final TextEditingController _emailController = TextEditingController();
+
+  late GlobalUIViewModel _ui;
+  late AuthViewModel _auth;
+  void resetPassword() async {
+    _ui.loadState(true);
+    try {
+      await _auth.resetPassword(_emailController.text).then((value) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text("Password reset link has been sent to your email.")));
+        Navigator.of(context).pop();
+      }).catchError((e) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text(e.message.toString())));
+      });
+    } catch (err) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(err.toString())));
+    }
+    _ui.loadState(false);
+  }
+
+  @override
+  void initState() {
+    _ui = Provider.of<GlobalUIViewModel>(context, listen: false);
+    _auth = Provider.of<AuthViewModel>(context, listen: false);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ForgotPasswordWidget();
+    return Scaffold(
+        backgroundColor: Color(0xFFAE1DFFD),
+        // appBar: AppBar(
+        // ),
+        body: SingleChildScrollView(
+          child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).viewPadding.top + 40,
+                  ),
+                  CustomIcon(icon: Icons.arrow_back),
+                  Container(
+                    padding: EdgeInsets.only(top: 50, bottom: 20),
+                    child: Text("Forgot Password",
+                        style: TextStyle(
+                          color: CustomTheme.darkGray,
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ),
+                  Text(
+                    "We’ll sent reset link to your email",
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 15,
+                      letterSpacing: 0.3,
+                      color: CustomTheme.darkGray,
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Container(
+                    child: Column(
+                      children: [
+                        CustomTextField(
+                          hintText: "Email address",
+                          controller: _emailController,
+                          prefixIcon: Icon(
+                            Icons.email,
+                            color: CustomTheme.lightTextColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  CustomButton(
+                    title: 'Reset',
+                    onPressed: () {
+                      resetPassword();
+                    },
+                  ),
+                ],
+              )),
+        ));
   }
 }
--- lib/repositories/auth_repositories.dart
+++ lib/repositories/auth_repositories.dart
@@ -1,25 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecommerce/models/user_model.dart';
 import 'package:ecommerce/services/firebase_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthRepository {
-  Future<UserCredential> register(UserModel user) async {
+  CollectionReference<UserModel> userRef =
+      FirebaseService.db.collection("users").withConverter<UserModel>(
+            fromFirestore: (snapshot, _) {
+              return UserModel.fromFirebaseSnapshot(snapshot);
+            },
+            toFirestore: (model, _) => model.toJson(),
+          );
+  Future<UserCredential?> register(UserModel user) async {
     try {
-      final response = await FirebaseService.db
-          .collection("users")
-          .where("username", isEqualTo: user.username!)
-          .get();
-      if (response.size != 0) throw Exception("Username already exists");
+      final response =
+          await userRef.where("username", isEqualTo: user.username!).get();
+      if (response.size != 0) {
+        throw Exception("Username already exists");
+      }
 
       UserCredential _uc = await FirebaseService.firebaseAuth
           .createUserWithEmailAndPassword(
-              email: user.email!,
-              password: user.password!); //create user with email and password
+              email: user.email!, password: user.password!);
 
       user.userId = _uc.user!.uid;
       user.fcmToken = "";
 
-      await FirebaseService.db.collection('users').add(user.toJson());
+      await userRef.add(user);
       return _uc;
     } catch (err) {
       rethrow;
@@ -31,6 +38,16 @@ class AuthRepository {
       UserCredential _uc = await FirebaseService.firebaseAuth
           .signInWithEmailAndPassword(email: email, password: password);
       return _uc;
+    } catch (err) {
+      rethrow;
+    }
+  }
+
+  Future<bool> resetPassword(String email) async {
+    try {
+      var res = await FirebaseService.firebaseAuth
+          .sendPasswordResetEmail(email: email);
+      return true;
     } catch (err) {
       rethrow;
     }
